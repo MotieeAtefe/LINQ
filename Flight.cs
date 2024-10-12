0dcff38efc99f49ebcd8f6d017b58ac8f91fade8
MotieeAtefe <motiee1377@gmaile.com>
2024-10-12 17:07:21 +0330

    Add project files.

---
+++ Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    internal class Flight
+    {
+        public string FlightNumber {get; set;}
+        public string DepartuerCity {get; set;}
+        public string ArrivalCity {get; set; }
+        public DateTime DepartuerTime {get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public override string ToString() =>
+
+            $"{FlightNumber} -" + $"{DepartuerCity} to {ArrivalCity}" +
+            $"-Departuer: {DepartuerTime},Arrival:{ArrivalTime}";
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        static void ListAllFligts(List<Flight> fligts)
+        {
+            Console.WriteLine("\nAllFlights: ");
+            fligts.ForEach(Console.WriteLine);
+        }
+        static void SearchFligt(List<Flight> fligts)
+        {
+            var matchingFlights = fligts.Where(f=>f.DepartuerCity.Equals())
+        }
+
+    }
+}
