2e0fb60adafa7f85963d10db6414f01d63c41889
MotieeAtefe <motiee1377@gmaile.com>
2024-10-12 18:28:16 +0330

    Printing each of flight List

--- Program.cs
+++ Program.cs
@@ -6,15 +6,7 @@
         {
             
         }
-        static void ListAllFligts(List<Flight> fligts)
-        {
-            Console.WriteLine("\nAllFlights: ");
-            fligts.ForEach(Console.WriteLine);
-        }
-        static void SearchFligt(List<Flight> fligts)
-        {
-            var matchingFlights = fligts.Where(f=>f.DepartuerCity.Equals())
-        }
+        
 
     }
 }
